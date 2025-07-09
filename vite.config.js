fd518874d6891ac67d136f6887e3aa727b70e1ea
joseaugusto <estoymuytristetriste@gmail.com>
2025-07-08 22:47:40 -0400

    Configure for new repository wilseco5585/page

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/pages/', // Base URL para GitHub Pages
+  base: '/page/', // Base URL para GitHub Pages - nombre del repositorio
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
